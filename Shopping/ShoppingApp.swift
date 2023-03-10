285f173bde62dcb03879830cdc1df0e9d4250622
emirmalika <emirmalika0712@gmail.com>
2023-03-10 13:03:18 +0600

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Malik Em on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
